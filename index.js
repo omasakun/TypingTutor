8fc758b5541834262bb7621d7cba20f586db16dc
omasakun <masaya2002@outlook.com>
2017-10-30 16:13:52 +0900

    changed the way to make question

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const nGram = 3;
  */
 var Chars = []; {
 	/**@type {Map<string,Map<number,number>>} */
-	let tmp = new Map();
+	let tmp = new Map(); // Map(nGram => Map(wordID=>count))
 	Words.forEach((word, index) => {
 		for (var i = 0; i < word.length; i++) {
 			for (var j = 1; j <= nGram; j++) {
@@ -32,7 +32,7 @@ var Chars = []; {
 			wordID.push([key, value]);
 			count += value;
 		});
-		let freq = Math.pow(count, key.length);
+		let freq = Math.pow(count, key.length); //TODO
 		maxFreq = Math.max(maxFreq, freq);
 		Chars.push({
 			text: key,
@@ -44,11 +44,11 @@ var Chars = []; {
 			},
 			wordID: wordID,
 			count: count,
-			freq: freq, //TODO
+			freq: freq,
 			score: undefined
 		});
 	});
-	Chars.forEach((item) => item.freq = item.freq / maxFreq); //TODO
+	Chars.forEach((item) => item.freq = item.freq / maxFreq);
 } // Set Chars
 /**
  * @type {{Date:string,Status:string,Words:number,Time:number,WPM:number,Acc:number}[]}
@@ -87,17 +87,75 @@ function MakeQuestions(resultCount) {
 	Chars.forEach((item) => {
 		var acc = item.pass / (item.fail + item.pass + 1);
 		var time = ((item.time.time + 1) / item.time.count);
-		item.score = time / acc; // 大きいと、やるべき
+		if (item.text == "the")
+			console.log();
+		item.score = time / acc; // 大きいと、やるべき (TODO: freqを無視している)
 	});
-
-	Chars.sort((a, b) => b.score != a.score ? b.score - a.score : b.freq - a.freq); // やるべきものが、先頭に来る
-	return Chars.slice(0, resultCount).map((v) => {
-		var index = (v.wordID.reduce((pv, v) => pv + v[1], 0) * Math.random()) << 0;
-		return Words[v.wordID[v.wordID.findIndex((vv) => {
-			index -= vv[1];
-			return index < 0;
-		})][0]];
-	});
+	/**@type {Map<string,{score:number,scoreInfs:number,freq:number,count:number}>} */
+	var wordIDs = new Map();
+	Chars.forEach(item => item.wordID.forEach((v) => {
+		var tmp = wordIDs.get(Words[v[0]]) || { score: 0, scoreInfs: 0, freq: 0, count: 0 };
+		if (Words[v[0]] == "the" && item.score == Infinity)
+			console.log();
+		if (item.score == Infinity)
+			tmp.scoreInfs += v[1];
+		else tmp.score += item.score * v[1];
+		tmp.freq += item.freq * v[1];
+		tmp.count+=v[1];
+		wordIDs.set(Words[v[0]], tmp);
+	}));
+	/**@type {string[]} */
+	var result = [];
+	var removed = new Set();
+	console.time("MakeQuestions");
+	// <=2つの/の間に*を入れて切り替わる
+	while (result.length < resultCount) { // 一回で、4.9% 0.7% 0.4% 0.3% 0.1%
+		let max = { score: -Infinity, scoreInfs: -Infinity, freq: -Infinity, text: "" };
+		wordIDs.forEach((v, key) => {
+			var compare = max.scoreInfs != v.scoreInfs / v.count ? max.scoreInfs - v.scoreInfs / v.count
+				: max.score != v.score / v.count ? max.score - v.score / v.count
+					: max.freq - v.freq / v.count;
+			if (compare > 0) return;
+			max = { score: v.score / v.count, scoreInfs: v.scoreInfs / v.count, freq: v.freq / v.count, text: key };
+		});
+		result.push(max.text);
+		for (var i = 0; i < max.text.length; i++) {
+			for (var j = 1; j <= nGram; j++) {
+				if (i + j - 1 >= max.text.length) break; // Out of array
+				let text = max.text.substr(i, j);
+				if (removed.has(text)) continue;
+				removed.add(text);
+				let Char = Chars.find(v => v.text == text);
+				if (Char == undefined) continue;
+				Char.wordID.forEach(v => {
+					var tmp = wordIDs.get(Words[v[0]]);
+					if (tmp == undefined) return;
+					if (Char.score == Infinity) tmp.scoreInfs -= v[1];
+					else tmp.score -= Char.score * v[1];
+					tmp.freq -= Char.freq * v[1];
+				});
+			}
+		}
+	}
+	/*/
+	result= Array.from(wordIDs).sort((a, b) => // 一回で、3.4% 1% 0.6% 0.4% 0.2%
+		b[1].scoreInfs != a[1].scoreInfs ? b[1].scoreInfs - a[1].scoreInfs
+			: b[1].score != a[1].score ? b[1].score - a[1].score
+				: b[1].freq - a[1].freq) // やるべきものを先頭に
+		.slice(0, resultCount).map(v => v[0]);
+	/**/
+	console.timeEnd("MakeQuestions");
+	return result;
+	/*/ 一回で、3.6% 1% 0.5% 0.4% 0.3%  | 3.5% 1% 0.6% 0.5% 0.3%
+		Chars.sort((a, b) => b.score != a.score ? b.score - a.score : b.freq - a.freq); // やるべきものが、先頭に来る
+		return Chars.slice(0, resultCount).map((v) => {
+			var index = (v.wordID.reduce((pv, v) => pv + v[1], 0) * Math.random()) << 0;
+			return Words[v.wordID[v.wordID.findIndex((vv) => {
+				index -= vv[1];
+				return index < 0;
+			})][0]];
+		});
+	*/
 }
 
 function MakePracticeText() {
@@ -206,7 +264,14 @@ function ShowStatus() {
 	_setList("WPMstrong", WPMtexts.reverse().slice(0, weakWordsCount));
 	_setList("ACCweak", ACCtexts.slice(0, weakWordsCount));
 	_setList("ACCstrong", ACCtexts.reverse().slice(0, weakWordsCount));
-
+	var playingStat = Chars.filter(v => v.text.length == 1).reduce((pv, cv) => ({ time: pv.time + cv.time.time, type: pv.type + cv.pass, typo: pv.typo + cv.fail }), { time: 0, type: 0, typo: 0 });
+	_set("totalTime", "Time: " + ms2text(playingStat.time));
+	_set("totalType", playingStat.type + " types");
+	_set("totalTypo", playingStat.typo + " typos");
+	function ms2text(ms) {
+		var ms = s = (ms << 0) / 1000, m = (ms / 60 / 1000) << 0, h = (ms / 60 / 60 / 1000) << 0;
+		return h + ":" + (m % 60) + ":" + s;
+	}
 	function _setList(id, arr) {
 		_(id).innerHTML = "";
 		arr.forEach((v) => {
@@ -251,7 +316,8 @@ window.addEventListener("load", () => {
 		})
 	});
 	_("download").addEventListener("click", () => {
-		Util.Download("keyboard.json", JSON.stringify(Chars.map((v) => [v.text, v.fail, v.pass, v.time.count, v.time.time << 0])));
+		var name = prompt("Filename...", "keyboard.json") || "keyboard.json";
+		Util.Download(name, JSON.stringify(Chars.map((v) => [v.text, v.fail, v.pass, v.time.count, v.time.time << 0])));
 	});
 	window.addEventListener("keypress", (e) => {
 		if (_("l1-Practice").innerText.startsWith(e.key.toLowerCase())) {
@@ -272,7 +338,7 @@ window.addEventListener("load", () => {
 				time = -1;
 			}
 			//MakeQuestions(0); // 下に依存する行がある
-			//_("line2").innerText = SliceLength(Chars.slice(0, 100).map((v) => v.text), 100).join(" ");
+			//_("line2").innerText = MakePracticeText();// SliceLength(Chars.slice(0, 100).map((v) => v.text), 100).join(" ");
 
 			queue2Log.push(item);
 			ShowStatus();
